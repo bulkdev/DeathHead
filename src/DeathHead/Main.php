74670291be1e9aab3010d7fa75136f0b6eafce13
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-06-05 19:30:40 -0400

    Done!
    Implemented small amount of code from Savion!

--- src/DeathHead/Main.php
+++ src/DeathHead/Main.php
@@ -8,6 +8,8 @@ use pocketmine\event\PlayerDeathEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item;
+use onebone\economyapi\EconomyAPI;
 
 class Main extends PluginBase{
 
@@ -15,14 +17,33 @@ public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 // maybe we dont need this?$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
 $this->getServer()->getLogger()->info(TextFormat::BLUE."DeathHead Enabled");
+$this->api = EconomyAPI::getInstance();
 }
 
 public function onDeath(PlayerDeathEvent $event){
   $cause = $event->getEntity()->getLastDamageCause();
         if($cause instanceof EntityDamageByEntityEvent) {
-            $player = $event->getEntity();
+            $player = $event->getEntity()
             $killer = $event->getEntity()->getLastDamageCause()->getDamager();
             if($killer instanceof Player) {
                     $killer->sendMessage($message);
+                    $sender->getInventory()->addItem($item);
+                    $damager->sendMessage("You killed $player.\nYou earn $" . $config->get("paid-amount") . for getting a kill.");
+			$player->sendMessage("You were killed by $damager.\nYou lose $" . $config->get("paid-amount") . " for getting killed.");
+			$this->api->setMoney($damager, $config->get("paid-amount");
+			$this->api->reduceMoney($player, $config->get("paid-amount");
                 }
 }
+public function onTouch(PlayerInteractEvent $event){
+            $player = $event->getPlayer()->getName();
+            $item = $event->getItem()->getName();
+            $config = $this->getConfig();
+	    if($item == "91") {
+	        $id = 91;
+                $damage = 0;
+                $count = 1;
+                $item =  Item::get($id, $damage, $count);
+                $player->getInventory()->addItem($item);
+                $player->sendMessage("You can sell this for" . $config->get("paid-amount") . "!");
+         }
+     }
