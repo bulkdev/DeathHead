a49ec0b7f41dcfaa9f361aa606b11ae03fc5d4c0
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-06-24 18:10:48 -0400

    Update Main.php

--- src/DeathHead/Main.php
+++ src/DeathHead/Main.php
@@ -1,5 +1,5 @@
 <?php
-//by: SavionLegendZzz and ItzBulkDev
+
 namespace DeathHead;
 
 use pocketmine\Server;
@@ -10,7 +10,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\item\Item;
-//use onebone\economyapi\EconomyAPI;
+use onebone\economyapi\EconomyAPI;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 
@@ -19,31 +19,34 @@ class Main extends PluginBase implements Listener {
 public function onEnable(){
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 $this->saveDefaultConfig();
-//$config = $this->getConfig();
+$config = $this->getConfig();
 $this->getServer()->getLogger()->info(TextFormat::BLUE."[DeathHead] DeathHead has been enabled!");
 $this->getServer()->getLogger()->info(TextFormat::BLUE."[DeathHead] Created by ItzBulkDev. Helped by SavionLegendZzz and MinecrafterPH");
-//$this->money = EconomyAPI::getInstance();
-//if (!$this->money) {
-	//$this->getLogger()->info(TextFormat::BLUE. "[DeathHead]" . TextFormat::RED . "Unable to find EconomyAPI.");
-	//return true;
-	//}
+$this->money = EconomyAPI::getInstance();
+if (!$this->money) {
+	$this->getLogger()->info(TextFormat::BLUE. "[DeathHead]" . TextFormat::RED . "Unable to find EconomyAPI.");
+	return true;
+	}
 }
 
 public function onDeath(PlayerDeathEvent $event){
+$config = $this->getConfig();
   $cause = $event->getEntity()->getLastDamageCause();
         if($cause instanceof EntityDamageByEntityEvent) {
             $player = $event->getEntity();
             $killer = $event->getEntity()->getLastDamageCause()->getDamager();
-            //$paid = $config->get("paid-amount");
-            //$lost = $config->get("lost-amount");
-            if($killer instanceof Player) {
+            $paid = $config->get("paid-amount");
+            $lost = $config->get("lost-amount");
+            $popups = $config->get("Enable-Popups")
+            $enabled = $config->get("Enable-Money");
+            if($killer instanceof Player and $enabled == true and $popups == true) {
                 $killer->getInventory()->addItem(Item::get("91"));
-                //$killer->sendPopup(TextFormat::GREEN."You earn $" . $paid . " for killing" . $player->getPlayer()->getName() . ".");
+                $killer->sendPopup(TextFormat::GREEN."You earn $" . $paid . " for killing" . $player->getPlayer()->getName() . ".");
                     
-		//$player->sendMessage(TextFormat::RED."You lose $" . $lost . " for getting killed by" . $killer->getPlayer()->getName(). ".");
+		$player->sendMessage(TextFormat::RED."You lose $" . $lost . " for getting killed by" . $killer->getPlayer()->getName(). ".");
 		
-		//$this->money->addMoney($killer, $config->get("paid-amount"));
-                //$this->money->reduceMoney($player, $config->get("lose-amount"));
+		$this->money->addMoney($killer, $paid);
+                $this->money->reduceMoney($player, $lost);
                 }
         }
 }
